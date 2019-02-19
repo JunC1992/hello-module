68061ba62b756609c6b25b47337689c7af0ff524
junc <chen.jun3232@gmail.com>
2019-02-19 03:21:01 -0500

    trivial commit

--- hello.go
+++ hello.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"rsc.io/quote"
 //	"hello-module/show"
+
+//	"github.com/JunC1992/easyStl/string"
 )
 
 func main() {
 //	show.Show()
+//	string.Introduce()
 	fmt.Println(quote.Hello())
 }
